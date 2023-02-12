d728c642f3823dcdda0eaa38314c0974dde52180
echemoo <echemoo@gmail.com>
2023-02-12 23:23:22 +0800

    完成生命周期回调

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,27 +2,43 @@ use std::collections::HashMap;
 
 pub struct Application {
     pub name: String,
-    _module: Modules,
+    modules: Modules,
+    index: usize,
 }
 
 impl Application {
     pub fn new() -> Application {
         Application {
             name: String::from("EAI"),
-            _module: Modules {
-                _ticks: HashMap::new(),
+            modules: Modules {
+                ticks: HashMap::new(),
             },
+            index: 0,
         }
     }
 
     pub fn run(&self) {
         println!("Hello World!");
+
+        self.modules.ticks.values().for_each(|module| {
+            module.init();
+        });
+        self.modules.ticks.values().for_each(|module| {
+            module.start();
+        });
+        self.modules.ticks.values().for_each(|module| {
+            module.end();
+        });
     }
 
-    pub fn _on(&self) -> usize {
-        0
+    pub fn on(&mut self, module: Box<dyn Tick>) -> usize {
+        self.index += 1;
+        self.modules.ticks.insert(self.index, module);
+        self.index
     }
-    pub fn _close(&self, _id: usize) {}
+    pub fn close(&mut self, id: usize) {
+        self.modules.ticks.remove(&id);
+    }
     pub fn _exec(&self) {}
 
     fn _run_all_room(&self) {}
@@ -37,5 +53,50 @@ pub trait Tick {
 }
 
 pub struct Modules {
-    pub _ticks: HashMap<usize, Box<dyn Tick>>,
+    pub ticks: HashMap<usize, Box<dyn Tick>>,
+}
+
+pub struct ModuleA;
+impl Tick for ModuleA {
+    fn init(&self) {
+        println!("ModuleA init");
+    }
+
+    fn start(&self) {
+        println!("ModuleA start");
+    }
+
+    fn end(&self) {
+        println!("ModuleA end");
+    }
+}
+
+pub struct ModuleB;
+impl Tick for ModuleB {
+    fn init(&self) {
+        println!("ModuleB init");
+    }
+
+    fn start(&self) {
+        println!("ModuleB start");
+    }
+
+    fn end(&self) {
+        println!("ModuleB end");
+    }
+}
+
+pub struct ModuleC;
+impl Tick for ModuleC {
+    fn init(&self) {
+        println!("ModuleC init");
+    }
+
+    fn start(&self) {
+        println!("ModuleC start");
+    }
+
+    fn end(&self) {
+        println!("ModuleC end");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 mod app;
 
 fn main() {
-    let app = app::Application::new();
+    let mut app = app::Application::new();
+
+    app.on(Box::new(app::ModuleA{}));
+    let id = app.on(Box::new(app::ModuleB{}));
+    app.on(Box::new(app::ModuleC{}));
+
+    app.run();
+
+    app.close(id);
 
     app.run();
 }
